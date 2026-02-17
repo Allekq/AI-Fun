7a52ca4cdcd1341ca8bba403b81f4b6a097cd566
alek <alekjaltuszyk@gmail.com>
2026-02-17 02:05:00 +0100

    Update models.py

--- src/ImageGen/models.py
+++ src/ImageGen/models.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class ImageModels(Enum):
     FLUX_KLEIN_4B = "x/flux2-klein:4b"
+    FLUX_KLEIN_9B = "x/flux2-klein:9b"
 
     def to_ollama_name(self) -> str:
         return self.value
